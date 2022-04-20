da15ad2114b81ad20ae468314c546f8782497d54
cosunae <carlos.osuna@env.ethz.ch>
2022-04-20 16:23:28 +0200

    formatting

--- idpi/test/test_thetav.py
+++ idpi/test/test_thetav.py
@@ -1,36 +1,35 @@
-import pytest
-import jinja2
-import subprocess
 import os
 import shutil
-import xarray as xr
+import subprocess
+
 import grib_decoder
+import jinja2
 import numpy as np
 import operators.thetav as mthetav
+import xarray as xr
 
 
 def test_thetav():
-    datadir = '/project/s83c/rz+/icon_data_processing_incubator/data'
-    datafile = datadir + '/lfff00000000'
-    datadir = '/project/s83c/rz+/icon_data_processing_incubator/data'
-    datafile = datadir + '/lfff00000000'
+    datadir = "/project/s83c/rz+/icon_data_processing_incubator/data"
+    datafile = datadir + "/lfff00000000"
+    datadir = "/project/s83c/rz+/icon_data_processing_incubator/data"
+    datafile = datadir + "/lfff00000000"
 
     ds = {}
-    grib_decoder.load_data(ds, ["P", "T", "QV"],
-                           datafile, chunk_size=None)
+    grib_decoder.load_data(ds, ["P", "T", "QV"], datafile, chunk_size=None)
 
     thetav = mthetav.fthetav(ds["P"], ds["T"], ds["QV"])
 
-    conf_files = {'inputi': "/scratch/cosuna/fieldextra_tests/lfff<DDHH>0000",
-                  'inputc': "/scratch/cosuna/fieldextra_tests/lfff00000000c",
-                  'output': "<HH>_THETAV.nc"
-                  }
-    out_file = '00_THETAV.nc'
-    prodfiles = ['fieldextra.diagnostic',
-                 'fieldextra.product', 'fieldextra.rmode']
+    conf_files = {
+        "inputi": "/scratch/cosuna/fieldextra_tests/lfff<DDHH>0000",
+        "inputc": "/scratch/cosuna/fieldextra_tests/lfff00000000c",
+        "output": "<HH>_THETAV.nc",
+    }
+    out_file = "00_THETAV.nc"
+    prodfiles = ["fieldextra.diagnostic", "fieldextra.product", "fieldextra.rmode"]
 
     testdir = os.path.dirname(os.path.realpath(__file__))
-    tmpdir = testdir + '/tmp'
+    tmpdir = testdir + "/tmp"
     cwd = os.getcwd()
 
     executable = "/project/s83c/fieldextra/tsa/bin/fieldextra_gnu_opt_omp"
@@ -39,24 +38,22 @@ def test_thetav():
     shutil.rmtree(tmpdir, ignore_errors=True)
     os.mkdir(tmpdir)
 
-    templateLoader = jinja2.FileSystemLoader(
-        searchpath=testdir+"/fe_templates")
+    templateLoader = jinja2.FileSystemLoader(searchpath=testdir + "/fe_templates")
     templateEnv = jinja2.Environment(loader=templateLoader)
-    template = templateEnv.get_template('./test_THETAV.nl')
+    template = templateEnv.get_template("./test_THETAV.nl")
     outputText = template.render(file=conf_files, ready_flags=tmpdir)
 
-    with open(tmpdir+'/test_THETAV.nl', 'w') as nl_file:
+    with open(tmpdir + "/test_THETAV.nl", "w") as nl_file:
         nl_file.write(outputText)
 
     # remove output and product files
     for afile in [out_file] + prodfiles:
-        if os.path.exists(cwd+'/'+afile):
-            os.remove(cwd+'/'+afile)
+        if os.path.exists(cwd + "/" + afile):
+            os.remove(cwd + "/" + afile)
 
-    subprocess.run(
-        [executable, tmpdir+'/test_THETAV.nl '], check=True)
+    subprocess.run([executable, tmpdir + "/test_THETAV.nl "], check=True)
 
-    fs_ds = xr.open_dataset('00_THETAV.nc')
+    fs_ds = xr.open_dataset("00_THETAV.nc")
 
     assert np.allclose(fs_ds["THETA_V"], thetav)
 

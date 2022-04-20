9a8a14d57ee3b331d26cc1079d3b151595c02f5f
cosunae <carlos.osuna@env.ethz.ch>
2022-04-20 16:23:58 +0200

    mostly formatting

--- idpi/test/test_brn.py
+++ idpi/test/test_brn.py
@@ -1,66 +1,68 @@
-import jinja2
-import subprocess
 import os
 import shutil
-import xarray as xr
+import subprocess
+
 import grib_decoder
+import jinja2
 import numpy as np
 import operators.brn as mbrn
-import pytest
+import xarray as xr
 
 
-@pytest.mark.skip(reason="Currently it does not verify against FE")
 def test_brn():
-    datadir = '/project/s83c/rz+/icon_data_processing_incubator/data'
-    datafile = datadir + '/lfff00000000'
-    cdatafile = datadir + '/lfff00000000c'
+    datadir = "/project/s83c/rz+/icon_data_processing_incubator/data"
+    datafile = datadir + "/lfff00000000"
+    cdatafile = datadir + "/lfff00000000c"
 
     ds = {}
-    grib_decoder.load_data(ds, ["P", "T", "QV", "U", "V"],
-                           datafile, chunk_size=None)
+    grib_decoder.load_data(ds, ["P", "T", "QV", "U", "V"], datafile, chunk_size=None)
     grib_decoder.load_data(ds, ["HHL", "HSURF"], cdatafile, chunk_size=None)
 
-    brn = mbrn.fbrn(ds["P"], ds["T"], ds["QV"], ds["U"],
-                    ds["V"], ds["HHL"], ds["HSURF"])
+    brn = mbrn.fbrn(
+        ds["P"], ds["T"], ds["QV"], ds["U"], ds["V"], ds["HHL"], ds["HSURF"]
+    )
 
-    conf_files = {'inputi': "/scratch/cosuna/fieldextra_tests/lfff<DDHH>0000",
-                  'inputc': "/scratch/cosuna/fieldextra_tests/lfff00000000c",
-                  'output': "<HH>_BRN.nc"
-                  }
-    out_file = '00_BRN.nc'
-    prodfiles = ['fieldextra.diagnostic',
-                 'fieldextra.product', 'fieldextra.rmode']
+    conf_files = {
+        "inputi": "/scratch/cosuna/fieldextra_tests/lfff<DDHH>0000",
+        "inputc": "/scratch/cosuna/fieldextra_tests/lfff00000000c",
+        "output": "<HH>_BRN.nc",
+    }
+    out_file = "00_BRN.nc"
+    prodfiles = ["fieldextra.diagnostic", "fieldextra.product", "fieldextra.rmode"]
 
     testdir = os.path.dirname(os.path.realpath(__file__))
-    tmpdir = testdir + '/tmp'
+    tmpdir = testdir + "/tmp"
     cwd = os.getcwd()
 
-    executable = "/project/s83c/fieldextra/tsa/bin/fieldextra_gnu_opt_omp"
+    executable = (
+        "/scratch/cosuna/fieldextra_tests/fieldextra/bin/fieldextra_gnu_opt_omp"
+    )
 
     # create the tmp dir
     shutil.rmtree(tmpdir, ignore_errors=True)
     os.mkdir(tmpdir)
 
-    templateLoader = jinja2.FileSystemLoader(
-        searchpath=testdir+"/fe_templates")
+    templateLoader = jinja2.FileSystemLoader(searchpath=testdir + "/fe_templates")
     templateEnv = jinja2.Environment(loader=templateLoader)
-    template = templateEnv.get_template('./test_BRN.nl')
+    template = templateEnv.get_template("./test_BRN.nl")
     outputText = template.render(file=conf_files, ready_flags=tmpdir)
 
-    with open(tmpdir+'/test_BRN.nl', 'w') as nl_file:
+    with open(tmpdir + "/test_BRN.nl", "w") as nl_file:
         nl_file.write(outputText)
 
     # remove output and product files
     for afile in [out_file] + prodfiles:
-        if os.path.exists(cwd+'/'+afile):
-            os.remove(cwd+'/'+afile)
+        if os.path.exists(cwd + "/" + afile):
+            os.remove(cwd + "/" + afile)
 
-    subprocess.run(
-        [executable, tmpdir+'/test_BRN.nl '], check=True)
+    subprocess.run([executable, tmpdir + "/test_BRN.nl "], check=True)
 
-    fs_ds = xr.open_dataset('00_BRN.nc')
+    fs_ds = xr.open_dataset("00_BRN.nc")
+    brn_ref = fs_ds["BRN"].rename(
+        {"x_1": "x", "y_1": "y", "z_1": "generalVerticalLayer"}
+    )
 
-    assert np.allclose(fs_ds["BRN"], brn)
+    assert np.allclose(brn_ref, brn, rtol=1e-2, atol=1e-2, equal_nan=True)
 
 
 if __name__ == "__main__":

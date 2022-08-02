48d1835d634d3854b18a736cae5faa51d9f5ec07
Petra Baumann <Petra.Baumann@meteoswiss.ch>
2022-08-02 17:58:27 +0200

    consider case that hzerocl is located below the model ground surface

--- idpi/src/operators/hzerocl.py
+++ idpi/src/operators/hzerocl.py
@@ -1,10 +1,12 @@
-"""algorithm for height of zero degree operator."""
+"""algorithm for computation of height of zero degree isotherm (without extrapolation below model orography)."""
 from operators.destagger import destagger
+import xarray as xr
+import numpy as np
 
 
 def fhzerocl(t, hhl):
     t0 = 273.15
-    hhl_fl = destagger(hhl, "generalVerticalLayer")
+    hfl = destagger(hhl, "generalVerticalLayer")
     tkm1 = t.copy()
     tkm1[{"generalVerticalLayer": slice(1, None)}] = t[
         {"generalVerticalLayer": slice(0, -1)}
@@ -27,31 +29,33 @@ def fhzerocl(t, hhl):
         }
     )
 
+    # Initiailize hzerocl with missing values
+    hzerocl = xr.full_like(hfl[{"generalVerticalLayer": 0}], np.nan)
+
     # 3d field with values of height for those levels where temperature is > 0 and it was
-    # < 0 on the level below. Otherwise values are NaN
-    height2 = hhl_fl.where((t >= t0) & (tkm1 < t0), drop=True)
+    # < 0 on the level below. Otherwise values are NaN.
+    height2 = hfl.where((t >= t0) & (tkm1 < t0), drop=True)
+    if height2.size > 0:
+        # The previous condition can be satisfied on multiple levels.
+        # Take the k indices of the maximum height value where the condition is satisfied
+        maxind = height2.fillna(-1).argmax(dim=["generalVerticalLayer"])
+        # compute the 2D fields with height values where T is > 0 and < 0 on level below
+        height2 = height2[{"generalVerticalLayer": maxind["generalVerticalLayer"]}]
+        # compute the 2D fields with height values where T is < 0 and > 0 on level above
+        height1 = hfl.where((tkp1 >= t0) & (t < t0), drop=True)[
+            {"generalVerticalLayer": maxind["generalVerticalLayer"]}
+        ]
+        # the height leve where T == 0 must be between [height1, height2]
+        t1 = t.where((t >= t0) & (tkm1 < t0), drop=True)[
+            {"generalVerticalLayer": maxind["generalVerticalLayer"]}
+        ]
 
-    # The previous condition can be satisfied on multiple levels.
-    # Take the K indices of the maximum height value where the condition is satisfied
-    maxind = height2.fillna(-1).argmax(dim=["generalVerticalLayer"])
-    # compute the 2D fields with height values where T is > 0 and < 0 on level below
-    height2 = height2[{"generalVerticalLayer": maxind["generalVerticalLayer"]}]
-    # compute the 2D fields with height values where T is < 0 and > 0 on level above
-    height1 = hhl_fl.where((tkp1 >= t0) & (t < t0), drop=True)[
-        {"generalVerticalLayer": maxind["generalVerticalLayer"]}
-    ]
-    # the height leve where T == 0 must be between [height1, height2]
+        t2 = t.where((tkp1 >= t0) & (t < t0), drop=True)[
+            {"generalVerticalLayer": maxind["generalVerticalLayer"]}
+        ]
+        w1 = (t1 - t0) / (t1 - t2)
+        w2 = (t0 - t2) / (t1 - t2)
 
-    t1 = t.where((t >= t0) & (tkm1 < t0), drop=True)[
-        {"generalVerticalLayer": maxind["generalVerticalLayer"]}
-    ]
+        hzerocl = w1 * height1 + w2 * height2
 
-    t2 = t.where((tkp1 >= t0) & (t < t0), drop=True)[
-        {"generalVerticalLayer": maxind["generalVerticalLayer"]}
-    ]
-    w1 = (t1 - t0) / (t1 - t2)
-    w2 = (t0 - t2) / (t1 - t2)
-
-    cond = w1 * height1 + w2 * height2
-
-    return cond
+    return hzerocl

16ba4e667644709b3971095557838ab6c45c1ec1
cosunae <carlos.osuna@meteoswiss.ch>
2023-10-05 11:35:22 +0200

    add relhum (#74)
    ## Purpose
    
    Add a relative humidity operator. Notice that there are several implementation in FE reference, depending on the available input fields and 3D vs 2D. Only the code path required for implementing the meteo_friuli_venezia (cosmo-2e) was implemented.
    
    ## Code changes:
    
    - Add relhum operator
    - Add a relhum operator test

---
+++ src/idpi/operators/atmo.py
@@ -0,0 +1,66 @@
+"""Atmospheric thermodynamic functions."""
+# Third-party
+import numpy as np
+
+# First-party
+from idpi.constants import pc_b1
+from idpi.constants import pc_b2w
+from idpi.constants import pc_b3
+from idpi.constants import pc_b4w
+from idpi.constants import pc_o_rdv
+from idpi.constants import pc_rdv
+
+
+def pv_sw(t):
+    """Pressure of water vapor at equilibrium over liquid water.
+
+    Parameters
+    ----------
+    t : xr.DataArray
+        temperature (in Kelvin)
+
+    Returns
+    -------
+    xr.DataArray
+        pressure of water vapor in Pascal
+
+    """
+    return pc_b1 * np.exp(pc_b2w * (t - pc_b3) / (t - pc_b4w))
+
+
+def qv_pvp(pv, p):
+    """Specific water vapor content (from perfect gas law and approximating q~w).
+
+    Parameters
+    ----------
+    pv : xr.DataArray
+        pressure of water vapor
+    p : xr.DataArray
+        pressure
+
+    Returns
+    -------
+    xr.DataArray
+        specific water vapor (dimensionless)
+
+    """
+    return pc_rdv * pv / np.maximum((p - pc_o_rdv * pv), 1.0)
+
+
+def pv_qp(qv, p):
+    """Partial pressure of water vapor (from perfect gas law and approximating q~w).
+
+    Parameters
+    ----------
+    qv : xr.DataArray
+        water vapor mixing ratio
+    p : xr.DataArray
+        pressure
+
+    Returns
+    -------
+    xr.DataArray
+        partial pressure of water vapor (same unit as p)
+
+    """
+    return qv * p / (pc_rdv + pc_o_rdv * qv)
---
+++ tests/test_idpi/test_relhum.py
@@ -0,0 +1,20 @@
+# Third-party
+from numpy.testing import assert_allclose
+
+# First-party
+from idpi.grib_decoder import GribReader
+from idpi.operators.relhum import relhum
+
+
+def test_relhum(data_dir, fieldextra):
+    datafile = data_dir / "lfff00000000.ch"
+    cdatafile = data_dir / "lfff00000000c.ch"
+
+    reader = GribReader([cdatafile, datafile], ref_param="P")
+    ds = reader.load_cosmo_data(["P", "T", "QV"])
+
+    relhum_arr = relhum(ds["QV"], ds["T"], ds["P"], clipping=True, phase="water")
+
+    fs_ds = fieldextra("RELHUM")
+
+    assert_allclose(fs_ds["RELHUM"], relhum_arr, rtol=5e-3, atol=5e-2)

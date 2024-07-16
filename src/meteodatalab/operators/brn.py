0ad0a3c0b7ad8fb36aaa0ba2ee3c69ec27317189
Nina Burgdorfer <83002751+ninaburg@users.noreply.github.com>
2024-07-16 17:50:39 +0200

    Update metadata BRN operator (#31)

--- src/meteodatalab/operators/brn.py
+++ src/meteodatalab/operators/brn.py
@@ -5,13 +5,47 @@ import numpy as np
 import xarray as xr
 
 # Local
+from .. import metadata
 from .. import physical_constants as pc
 from .destagger import destagger
 from .thetav import fthetav
 
 
-def fbrn(p, t, qv, u, v, hhl, hsurf):
-    """Bulk Richardson Number (BRN)."""
+def fbrn(
+    p: xr.DataArray,
+    t: xr.DataArray,
+    qv: xr.DataArray,
+    u: xr.DataArray,
+    v: xr.DataArray,
+    hhl: xr.DataArray,
+    hsurf: xr.DataArray,
+) -> xr.DataArray:
+    """Bulk Richardson Number (BRN).
+
+    Parameters
+    ----------
+    p : xarray.DataArray
+        pressure in Pa
+    t : xarray.DataArray
+        air temperature in K
+    qv : xarray.DataArray
+        specific humidity (dimensionless)
+    u : xarray.DataArray
+        the x component of the wind velocity in m/s
+    v : xarray.DataArray
+        the y component of the wind velocity in m/s
+    hhl : xarray.DataArray
+        Heights of the interfaces between vertical layers in m amsl
+    hsurf : xarray.DataArray
+        earth surface height in m amsl
+
+
+    Returns
+    -------
+    xarray.DataArray
+        Bulk Richardson Number (dimensionless)
+
+    """
     nlevels = p.sizes["z"]
 
     thetav = fthetav(p, t, qv)
@@ -34,4 +68,7 @@ def fbrn(p, t, qv, u, v, hhl, hsurf):
         / (thetav_sum * (u_**2 + v_**2))
     )
 
-    return brn
+    return xr.DataArray(
+        data=brn,
+        attrs=metadata.override(p.message, shortName="BRN"),
+    )

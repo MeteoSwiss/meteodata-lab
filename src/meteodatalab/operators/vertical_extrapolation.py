b32a91a11b2720c723acdb13bfc96cbe2286f023
Francesco Zanetta <zanetta.francesco@gmail.com>
2025-04-07 15:58:55 +0200

    use surface height instead of geopotential and add units in docstrings

--- src/meteodatalab/operators/vertical_extrapolation.py
+++ src/meteodatalab/operators/vertical_extrapolation.py
@@ -15,7 +15,7 @@ T1 = 298.0
 
 def extrapolate_temperature_sfc2p(
     t_sfc: xr.DataArray,
-    z_sfc: xr.DataArray,
+    h_sfc: xr.DataArray,
     p_sfc: xr.DataArray,
     p_target: float,
 ) -> xr.DataArray:
@@ -24,19 +24,19 @@ def extrapolate_temperature_sfc2p(
     Implements the algorithm described in [1]_. The algorithm extrapolates
     temperature from the surface to a target pressure level using a
     polynomial expression of a dimensionless variable y, which is a function of
-    the surface temperature, surface pressure, and geopotential. It assumes
+    the surface temperature, surface pressure, and height. It assumes
     a constant lapse rate of 0.0065 K m^-1.
 
     Parameters
     ----------
     t_sfc : xr.DataArray
         Surface temperature [K].
-    z_sfc : xr.DataArray
-        Surface geopotential.
+    h_sfc : xr.DataArray
+        Surface height [m].
     p_sfc : xr.DataArray
-        Surface pressure.
+        Surface pressure [Pa].
     p_target : float
-        Target pressure level.
+        Target pressure level [Pa].
 
     Returns
     -------
@@ -48,12 +48,12 @@ def extrapolate_temperature_sfc2p(
     .. [1] https://www.umr-cnrm.fr/gmapdoc/IMG/pdf/ykfpos46t1r1.pdf
 
     """
-    y = _vertical_extrapolation_y_term(t_sfc, p_sfc, z_sfc, p_target)
+    y = _vertical_extrapolation_y_term(t_sfc, p_sfc, h_sfc, p_target)
     return t_sfc * (1 + y + (y**2) / 2 + (y**3) / 6)
 
 
 def extrapolate_geopotential_sfc2p(
-    z_sfc: xr.DataArray,
+    h_sfc: xr.DataArray,
     t_sfc: xr.DataArray,
     p_sfc: xr.DataArray,
     p_target: float,
@@ -63,19 +63,19 @@ def extrapolate_geopotential_sfc2p(
     Implements the algorithm described in [1]_. The algorithm extrapolates
     geopotential from the surface to a target pressure level using a
     polynomial expression of a dimensionless variable y, which is a function of
-    the surface temperature, surface pressure, and geopotential. It assumes
+    the surface temperature, surface pressure, and height. It assumes
     a constant lapse rate of 0.0065 K m^-1.
 
     Parameters
     ----------
-    z_sfc : xr.DataArray
-        Surface geopotential.
+    h_sfc : xr.DataArray
+        Surface height [m].
     t_sfc : xr.DataArray
-        Surface temperature.
+        Surface temperature [K].
     p_sfc : xr.DataArray
-        Surface pressure.
+        Surface pressure [Pa].
     p_target : float
-        Target pressure level.
+        Target pressure level [Pa].
 
     Returns
     -------
@@ -88,27 +88,29 @@ def extrapolate_geopotential_sfc2p(
 
     """
     y = _vertical_extrapolation_y_term(
-        t_sfc, p_sfc, z_sfc, p_target, lapse_rate=LAPSE_RATE
+        t_sfc, p_sfc, h_sfc, p_target, lapse_rate=LAPSE_RATE
     )
-    return z_sfc - pc.r_d * t_sfc * np.log(p_target / p_sfc) * (1 + y / 2 + (y**2) / 6)
+    return h_sfc * pc.g - pc.r_d * t_sfc * np.log(p_target / p_sfc) * (
+        1 + y / 2 + (y**2) / 6
+    )
 
 
-def _vertical_extrapolation_t_zero_prime(t_sfc, z_sfc):
-    t = t_sfc + LAPSE_RATE * z_sfc / pc.g
+def _vertical_extrapolation_t_zero_prime(t_sfc, h_sfc):
+    t = t_sfc + LAPSE_RATE * h_sfc
     t_min = np.minimum(t, T1)
-    return xr.where(z_sfc / pc.g > H2, t_min, t_min * 0.5 + t * 0.5)
+    return xr.where(h_sfc > H2, t_min, t_min * 0.5 + t * 0.5)
 
 
-def _vertical_extrapolation_lapse_rate(z_sfc, t_sfc):
-    t_zero_prime = _vertical_extrapolation_t_zero_prime(t_sfc, z_sfc)
+def _vertical_extrapolation_lapse_rate(h_sfc, t_sfc):
+    t_zero_prime = _vertical_extrapolation_t_zero_prime(t_sfc, h_sfc)
     return xr.where(
-        z_sfc / pc.g < H1,
+        h_sfc < H1,
         LAPSE_RATE,
-        pc.g / z_sfc * np.maximum(t_zero_prime - t_sfc, 0.0),
+        1 / h_sfc * np.maximum(t_zero_prime - t_sfc, 0.0),
     )
 
 
-def _vertical_extrapolation_y_term(t_sfc, p_sfc, z_sfc, p_target, lapse_rate=None):
+def _vertical_extrapolation_y_term(t_sfc, p_sfc, h_sfc, p_target, lapse_rate=None):
     if lapse_rate is None:
-        lapse_rate = _vertical_extrapolation_lapse_rate(z_sfc, t_sfc)
+        lapse_rate = _vertical_extrapolation_lapse_rate(h_sfc, t_sfc)
     return lapse_rate * pc.r_d / pc.g * np.log(p_target / p_sfc)

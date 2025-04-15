1a6b8d5091da715b78ea2ed0bf28265bf623e21f
Francesco Zanetta <62377868+frazane@users.noreply.github.com>
2025-04-15 12:00:44 +0200

    Vertical extrapolation operators (#74)
    ## Purpose
    
    Closes #73 by adding operators to perform vertical extrapolation.
    
    ## Code changes:
    
    - added new module `meteodatalab/operators/vertical_extrapolation.py`
    with the functions needed to perform vertical extrapolation of
    geopotential and temperature
    - added test for the two main extrapolation functions
    
    ---------
    
    Co-authored-by: Christian Kanesan <christian.kanesan@meteoswiss.ch>

---
+++ src/meteodatalab/operators/vertical_extrapolation.py
@@ -0,0 +1,155 @@
+"""Vertical extrapolation operators."""
+
+# Third-party
+import numpy as np
+import xarray as xr
+
+# Local
+from .. import metadata
+from .. import physical_constants as pc
+
+LAPSE_RATE = 0.0065  # K m^-1
+H1 = 2000.0
+H2 = 2500.0
+T1 = 298.0
+
+
+def extrapolate_temperature_sfc2p(
+    t_sfc: xr.DataArray,
+    h_sfc: xr.DataArray,
+    p_sfc: xr.DataArray,
+    p_target: float,
+) -> xr.DataArray:
+    """Extrapolate temperature to a target pressure level.
+
+    Implements the algorithm described in [1]_. The algorithm extrapolates
+    temperature from the surface to a target pressure level using a
+    polynomial expression of a dimensionless variable y, which is a function of
+    the surface temperature, surface pressure, and height. It assumes
+    a constant lapse rate of 0.0065 K m^-1 and dry air gas constant.
+
+    .. caution :
+        This extrapolation should be used with caution. Its intended use is to
+        extrapolate temperature to pressure levels below the surface, where
+        values are undefined. This is useful for applications where no missing values
+        are allowed, such as when training data-driven models. Results of the
+        extrapolation are not physically meaningful.
+
+    Parameters
+    ----------
+    t_sfc : xr.DataArray
+        Surface temperature [K].
+    h_sfc : xr.DataArray
+        Surface height [m].
+    p_sfc : xr.DataArray
+        Surface pressure [Pa].
+    p_target : float
+        Target pressure level [Pa].
+
+    Returns
+    -------
+    xr.DataArray
+        Extrapolated temperature at the target pressure level.
+
+    References
+    ----------
+    .. [1] https://www.umr-cnrm.fr/gmapdoc/IMG/pdf/ykfpos46t1r1.pdf
+
+    """
+    y = _vertical_extrapolation_y_term(t_sfc, p_sfc, h_sfc, p_target)
+    res = t_sfc * (1 + y + (y**2) / 2 + (y**3) / 6)
+    res.attrs = metadata.override(
+        t_sfc.metadata, shortName="T", typeOfLevel="isobaricInPa"
+    )
+    res = _assign_vcoord(res, p_target)
+    return res
+
+
+def extrapolate_geopotential_sfc2p(
+    h_sfc: xr.DataArray,
+    t_sfc: xr.DataArray,
+    p_sfc: xr.DataArray,
+    p_target: float,
+) -> xr.DataArray:
+    """Extrapolate geopotential to a target pressure level.
+
+    Implements the algorithm described in [1]_. The algorithm extrapolates
+    geopotential from the surface to a target pressure level using a
+    polynomial expression of a dimensionless variable y, which is a function of
+    the surface temperature, surface pressure, and height. It assumes
+    a constant lapse rate of 0.0065 K m^-1 and dry air gas constant.
+
+    .. caution :
+        This extrapolation should be used with caution. Its intended use is to
+        extrapolate geopotential to pressure levels below the surface, where
+        values are undefined. This is useful for applications where no missing values
+        are allowed, such as when training data-driven models. Results of the
+        extrapolation are not physically meaningful.
+
+    Parameters
+    ----------
+    h_sfc : xr.DataArray
+        Surface height [m].
+    t_sfc : xr.DataArray
+        Surface temperature [K].
+    p_sfc : xr.DataArray
+        Surface pressure [Pa].
+    p_target : float
+        Target pressure level [Pa].
+
+    Returns
+    -------
+    xr.DataArray
+        Extrapolated geopotential at the target pressure level.
+
+    References
+    ----------
+    .. [1] https://www.umr-cnrm.fr/gmapdoc/IMG/pdf/ykfpos46t1r1.pdf
+
+    """
+    y = _vertical_extrapolation_y_term(
+        t_sfc, p_sfc, h_sfc, p_target, lapse_rate=LAPSE_RATE
+    )
+    res = h_sfc * pc.g - pc.r_d * t_sfc * np.log(p_target / p_sfc) * (
+        1 + y / 2 + (y**2) / 6
+    )
+    res.attrs = metadata.override(
+        t_sfc.metadata, shortName="FI", typeOfLevel="isobaricInPa"
+    )
+    res = _assign_vcoord(res, p_target)
+    return res
+
+
+def _vertical_extrapolation_t_zero_prime(t_sfc, h_sfc):
+    t = t_sfc + LAPSE_RATE * h_sfc
+    t_min = np.minimum(t, T1)
+    return xr.where(h_sfc > H2, t_min, t_min * 0.5 + t * 0.5)
+
+
+def _vertical_extrapolation_lapse_rate(h_sfc, t_sfc):
+    t_zero_prime = _vertical_extrapolation_t_zero_prime(t_sfc, h_sfc)
+    return xr.where(
+        h_sfc < H1,
+        LAPSE_RATE,
+        1 / h_sfc * np.maximum(t_zero_prime - t_sfc, 0.0),
+    )
+
+
+def _vertical_extrapolation_y_term(
+    t_sfc, p_sfc, h_sfc, p_target, lapse_rate=None
+) -> xr.DataArray:
+    if lapse_rate is None:
+        lapse_rate = _vertical_extrapolation_lapse_rate(h_sfc, t_sfc)
+    return lapse_rate * pc.r_d / pc.g * np.log(p_target / p_sfc)
+
+
+def _assign_vcoord(x: xr.DataArray, p_target: float) -> xr.DataArray:
+    attrs = {
+        "units": "Pa",
+        "positive": "down",
+        "standard_name": "air_pressure",
+        "long_name": "pressure",
+    }
+    x = x.assign_coords(z=[p_target])
+    x["z"].attrs = attrs
+    return x
---
+++ tests/test_meteodatalab/test_extpl.py
@@ -0,0 +1,58 @@
+# Third-party
+from numpy.testing import assert_allclose
+
+# First-party
+import meteodatalab.physical_constants as pc
+from meteodatalab import data_source, grib_decoder
+from meteodatalab.metadata import override, set_origin_xy
+from meteodatalab.operators.destagger import destagger
+from meteodatalab.operators.vertical_extrapolation import (
+    extrapolate_geopotential_sfc2p,
+    extrapolate_temperature_sfc2p,
+)
+from meteodatalab.operators.vertical_interpolation import interpolate_k2p
+
+
+def test_extrapolate_sfc2p(data_dir):
+
+    # define target coordinates
+    target_p = 850.0
+
+    # input data
+    files = [
+        data_dir / "COSMO-1E/1h/ml_sl/000/lfff00000000",
+        data_dir / "COSMO-1E/1h/const/000/lfff00000000c",
+    ]
+
+    # load input data set
+    fds = data_source.FileDataSource(datafiles=files)
+    ds = grib_decoder.load(fds, {"param": ["HSURF", "T_2M", "PS", "HHL", "T", "P"]})
+    set_origin_xy(ds, ref_param="HHL")
+    hfl = destagger(ds["HHL"], "z")
+    fi = (hfl * pc.g).assign_attrs(override(hfl.attrs["metadata"], shortName="FI"))
+
+    # call extrapolation operator for geopotential
+    expected = interpolate_k2p(fi, "linear_in_lnp", ds["P"], [target_p], "hPa").squeeze(
+        "z"
+    )
+    res = (
+        extrapolate_geopotential_sfc2p(
+            ds["HSURF"], ds["T_2M"], ds["PS"], target_p * 100.0
+        )
+        .squeeze("z")
+        .where(~expected.isnull())
+    )
+    assert_allclose(res, expected, rtol=0.04)
+
+    # call extrapolation operator for temperature
+    expected = interpolate_k2p(
+        ds["T"], "linear_in_lnp", ds["P"], [target_p], "hPa"
+    ).squeeze("z")
+    res = (
+        extrapolate_temperature_sfc2p(
+            ds["T_2M"], ds["HSURF"], ds["PS"], target_p * 100.0
+        )
+        .squeeze("z")
+        .where(~expected.isnull())
+    )
+    assert_allclose(res, expected, rtol=0.04)

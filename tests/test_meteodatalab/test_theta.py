81aa50c052653e8106aadd5ba0c2eb7de48048a7
mitTheos <tim2kaspar@gmail.com>
2025-06-17 15:27:32 +0200

    update tests with error margin to fieldextra

--- tests/test_meteodatalab/test_theta.py
+++ tests/test_meteodatalab/test_theta.py
@@ -5,6 +5,16 @@ from numpy.testing import assert_allclose
 from meteodatalab.grib_decoder import GribReader
 from meteodatalab.operators.theta import compute_theta
 
+def calculate_error(p, t):
+    p0 = 1.0e5 # Reference surface pressure 
+    r_d = 287.05  # Specific gas constant for dry air [J kg-1 K-1]
+    cp_d = 1005.0  # Specific heat capacity at constant pressure for dry air [J kg-1 K]
+    rdocp = r_d / cp_d
+    kappa = 0.285691
+
+    p_np = p.values
+    t_np = t.values
+    return (p0/p_np) ** rdocp * t_np - (p0/p_np) ** kappa * t_np
 
 def test_theta(data_dir, fieldextra):
     datafile = data_dir / "COSMO-1E/1h/ml_sl/000/lfff00000000"
@@ -24,4 +34,7 @@ def test_theta(data_dir, fieldextra):
 
     fs_ds = fieldextra("THETA")
 
-    assert_allclose(fs_ds["THETA"], theta, rtol=1e-6)
+    # due to the difference in constants from earthkit-meteo and fieldextra theres a increased margin of error
+    err = calculate_error(ds["P"], ds["T"])
+
+    assert_allclose(fs_ds["THETA"] - err, theta, rtol=1e-6)

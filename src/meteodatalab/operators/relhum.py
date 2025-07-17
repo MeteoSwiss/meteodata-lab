d0970c557418b7702cec0faa1c271a2bf05f670e
Nina Burgdorfer <83002751+ninaburg@users.noreply.github.com>
2025-07-17 09:52:20 +0200

    Replace `relhum` operator with earthkit-meteo operator (#115)
    ## Purpose
    Replaced the internal computation in meteodata-lab with the
    implementation from earthkit-meteo (see
    [source](https://github.com/ecmwf/earthkit-meteo/blob/f2dae217d52a3361b30d99a178fa857308e4ed99/src/earthkit/meteo/thermo/array/thermo.py#L524))
    
    Additional infos:
    - To ensure the tests pass, I had to increase the relative tolerance
    from 5e-3 to 7e-3, as the test cases for phase="water" and
    phase="water+ice" were failing with the original threshold.
    - I did not use the [relative humidity operator from
    ek-meteo](https://github.com/ecmwf/earthkit-meteo/blob/f2dae217d52a3361b30d99a178fa857308e4ed99/src/earthkit/meteo/thermo/array/thermo.py#L524)
    directly because it does not support the phase argument. Instead, I
    invoked the underlying subfunctions, which do allow the phase to be
    specified.
    
    
    Jira task: https://meteoswiss.atlassian.net/browse/APNRZ-728
    
    ---------
    
    Co-authored-by: Nina Burgdorfer <nina.burgdorfer@meteoswiss.ch>

--- src/meteodatalab/operators/relhum.py
+++ src/meteodatalab/operators/relhum.py
@@ -5,21 +5,21 @@ from typing import Literal
 
 # Third-party
 import xarray as xr
+from earthkit.meteo import thermo  # type: ignore
 
 # Local
 from .. import metadata
-from .atmo import pv_si, pv_sm, pv_sw, qv_pvp
 
 
 def relhum(
-    qv, t, p, clipping=True, phase: Literal["water", "ice", "water+ice"] = "water"
+    w, t, p, clipping=True, phase: Literal["water", "ice", "water+ice"] = "water"
 ):
     """Calculate relative humidity.
 
     Parameters
     ----------
-    qv : xarray.DataArray
-        water vapor mixing ratio
+    w : xarray.DataArray
+        water vapor mixing ratio in kg/kg
     t : xarray.DataArray
         temperature in Kelvin
     p : xarray.DataArray
@@ -28,7 +28,7 @@ def relhum(
         clips the relative humidity to [0,100] interval.
         Only upper bound is controlled by this parameter,
         since lower bound clipping is always performed.
-    phase : Literal["water", "ice", "water+ic"]
+    phase : Literal["water", "ice", "water+ice"]
         Customizes how relative humidity is computed.
         'water'        over water
         'ice'          over ice
@@ -42,19 +42,43 @@ def relhum(
     """
     max = 100 if clipping else None
 
+    # Phase-specific metadata and saturation vapor pressure configuration
     phase_conditions = {
-        "water": {"func": pv_sw(t), "shortName": "RELHUM"},
-        "ice": {"func": pv_si(t), "shortName": "RH_ICE"},
-        "water+ice": {"func": pv_sm(t), "shortName": "RH_MIX_EC"},
+        "water": {
+            "shortName": "RELHUM",
+            "svp_phase": "water",
+        },
+        "ice": {
+            "shortName": "RH_ICE",
+            "svp_phase": "ice",
+        },
+        "water+ice": {
+            "shortName": "RH_MIX_EC",
+            "svp_phase": "mixed",  # earthkit-meteo op. requires "mixed"
+        },
     }
 
     if phase not in phase_conditions:
         raise ValueError("Invalid phase. Phase must be 'water', 'ice', or 'water+ice'.")
 
-    result = (100.0 * qv / qv_pvp(phase_conditions[phase]["func"], p)).clip(0, max)
+    # Convert mixing ratio (w) to specific humidity (q)
+    q = w / (1 + w)
 
+    pb, tb, qb = xr.broadcast(p, t, q)
+
+    # Use the mapped saturation vapor pressure phase
+    svp_phase = phase_conditions[phase]["svp_phase"]
+
+    # Compute relative humidity using earthkit-meteo operators
+    rh = (
+        100
+        * thermo.vapour_pressure_from_specific_humidity(qb, pb)
+        / thermo.saturation_vapour_pressure(tb.values, phase=svp_phase)
+    ).clip(0, max)
+
+    # Return RH with appropriate metadata
     return xr.DataArray(
-        data=result,
+        data=rh,
         attrs=metadata.override(
             t.metadata, shortName=phase_conditions[phase]["shortName"]
         ),

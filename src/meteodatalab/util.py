42b0d971a245fc74d90ba738f706e4579baacbfe
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2025-07-17 09:35:54 +0200

    Implement weight cache (#113)
    ## Purpose
    
    Implement an in memory cache for the `iconremap` function. The cache
    keys are based on the field section 3 hash and the repr of the
    destination grid object. The cache size defaults to 10 instances and the
    least recently used key is evicted when the cache is full.

--- src/meteodatalab/util.py
+++ src/meteodatalab/util.py
@@ -1,11 +1,19 @@
 """Util module."""
 
 # Standard library
+import dataclasses as dc
+import enum
+import functools
+import heapq
+import itertools
 import logging
+import typing
+from collections.abc import Callable
 
 # Third-party
 import requests
 from requests.adapters import HTTPAdapter
+from typing_extensions import ParamSpec
 from urllib3.util import Retry
 
 
@@ -33,3 +41,93 @@ def init_session(logger: logging.Logger | None = None) -> requests.Session:
         session.hooks["response"].append(_log_errors)
 
     return session
+
+
+P = ParamSpec("P")
+K = typing.TypeVar("K", bound=typing.Hashable)
+T = typing.TypeVar("T")
+F = Callable[P, T]
+
+
+# Work around for sentinel object pending PEP 661
+class Removed(enum.Enum):
+    token = 0
+
+
+REMOVED = Removed.token
+
+
+@dc.dataclass(unsafe_hash=True, order=True)
+class Entry(typing.Generic[K]):
+    count: int
+    item: K | Removed
+
+
+class Queue(typing.Generic[K]):
+    # Adapted from priority queue example in heapq documentation
+
+    class Full(Exception):
+        """Exception indicating that the queue is full."""
+
+    def __init__(self, maxsize: int) -> None:
+        self.queue: list[Entry[K]] = []
+        self.finder: dict[K, Entry[K]] = {}
+        self.counter = itertools.count()
+        self.maxsize = maxsize
+        self.size = 0
+
+    def add_item(self, item: K) -> None:
+        if item in self.finder:
+            self.remove_item(item)
+        elif self.size >= self.maxsize:
+            raise self.Full
+        count = next(self.counter)
+        entry = Entry(count, item)
+        self.finder[item] = entry
+        heapq.heappush(self.queue, entry)
+        self.size += 1
+
+    def remove_item(self, item: K) -> None:
+        entry = self.finder.pop(item)
+        entry.item = REMOVED
+        self.size -= 1
+
+    def pop_item(self) -> K:
+        while self.queue:
+            item = heapq.heappop(self.queue).item
+            if item is not REMOVED:
+                del self.finder[item]
+                self.size -= 1
+                return item
+        raise KeyError("pop from empty queue")
+
+
+def memoize(key_maker: Callable[P, K], maxsize: int = 10) -> Callable[[F], F]:
+    if maxsize < 1:
+        raise ValueError("maxsize must be 1 or more")
+
+    def decorator(func: Callable[P, T]) -> Callable[P, T]:
+        cache: dict[K, T] = {}
+        queue: Queue[K] = Queue(maxsize=maxsize)
+
+        @functools.wraps(func)
+        def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
+            key = key_maker(*args, **kwargs)
+            if key is not None and key in cache:
+                queue.add_item(key)
+                return cache[key]
+
+            result = func(*args, **kwargs)
+            if key is not None:
+                cache[key] = result
+                try:
+                    queue.add_item(key)
+                except queue.Full:
+                    old_key = queue.pop_item()
+                    del cache[old_key]
+                    queue.add_item(key)
+            return result
+
+        return wrapped
+
+    return decorator
---
+++ tests/test_meteodatalab/test_util.py
@@ -0,0 +1,27 @@
+# Third-party
+import pytest
+
+# First-party
+from meteodatalab import util
+
+
+def test_queue():
+    queue = util.Queue(maxsize=5)
+    for i in range(5):
+        queue.add_item(i)
+
+    with pytest.raises(queue.Full):
+        queue.add_item(5)
+
+    queue.add_item(3)
+    assert queue.pop_item() == 0
+    queue.add_item(1)
+    assert queue.pop_item() == 2
+    assert queue.pop_item() == 4
+    assert queue.pop_item() == 3
+    assert queue.pop_item() == 1
+    queue.add_item(5)
+    assert queue.pop_item() == 5
+
+    with pytest.raises(KeyError):
+        queue.pop_item()
